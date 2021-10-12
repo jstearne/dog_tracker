adfb4d856f37ead583823984a9de507108042c75
Jared Stearne <jared.stearne@gmail.com>
2021-10-11 19:38:30 -0700

    Auth is working but not fully tested. Detail page is linked to dogs_list.html. Add Update/Remove functionality and the backend is basically done. After that, styling and UX.

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.Home.as_view(), name="home"), # index
     path('dogs/', views.DogList.as_view(), name="dog_list"), # new home page
     path('about/', views.About.as_view(), name="about"),
+    path('dogs/<int:pk>/', views.DogDetail.as_view(), name="dog_detail"),
     path('dogs/new/', views.DogCreate.as_view(), name="dog_create"),
     path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name="dog_update"),
     path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name="dog_delete"),
@@ -13,6 +14,7 @@ urlpatterns = [
     # DOESN'T WORK path('admin/', admin.site.urls),
     # path('', include('main_app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', views.Signup.as_view(), name="signup")
 ]
 
 
